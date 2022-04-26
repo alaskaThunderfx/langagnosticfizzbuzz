4f38592217a18c3655b77c8b8e382be6dce4032c
alaskaThunderfx <smackey85@yahoo.com>
2022-04-26 15:48:28 -0400

    Solve FizzBuzz challenge in multiple languages

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void FizzBuzz(int endNumber)
+{
+    for (int i = 1; i < endNumber + 1; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+FizzBuzz(30);
